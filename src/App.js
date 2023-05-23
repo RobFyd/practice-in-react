b2eff9becad9ed076040b264b36f21a9611d105a
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-05-24 00:59:55 +0100

    effect will only be triggered once

--- src/App.js
+++ src/App.js
@@ -4,22 +4,17 @@ import './App.css';
 function App() {
 
   const [count, setCount] = useState(0);
-  const [name, setName] = useState("");
 
   useEffect(() => {
-    document.title = `Counter: ${count}, name: ${name}`;
-  }, [count, name]);
+    setInterval(() => {
+      setCount(count => count + 1);
+    }, 1000);
+  }, []);
 
   return (
     <>
       <p>
         Counter: {count}
-      </p>
-      <p>
-        <button onClick={() => setCount(count => count + 1)}>+1</button>
-      </p>
-      <p>
-        Name: <input value={name} onChange={({ target }) => setName(target.value)} />
       </p>
     </>
   );
