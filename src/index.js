77bdc3b3d53870a18d424987f4bd8dc4c6b8e181
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-06-27 23:37:02 +0100

    tasksReducer added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { configureStore } from "@reduxjs/toolkit";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const initialState = {
+  tasks: [],
+};
+
+const tasksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "addTask":
+      return {
+        ...state,
+        tasks: [
+          ...state.tasks,
+          {
+            content: action.payload,
+          },
+        ],
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = configureStore({ reducer: tasksReducer });
+console.log(store.getState());
+
+const addTaskAction = {
+  type: "addTask",
+  payload: "learn reducer",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
