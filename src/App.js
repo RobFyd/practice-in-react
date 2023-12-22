3d04ff9e43b7b53adebbd7ca1978cf40fd36c512
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-12-22 00:15:06 +0000

    Refactor form structure and class names

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 const tasks = [
   {
     id: 1,
@@ -12,29 +11,29 @@ const tasks = [
   },
 ];
 
+let hideDoneTasks = false;
+
 function App() {
   return (
-    <>
+    <main>
       <h1>Tasks List</h1>
-      <form className="js-form">
-        <div>
-          <h2>Add task</h2>
-          <div>
-            <input className="form__field js-newTask" autoFocus />
-            <button className="form__button js-addTask">Add new task</button>
-          </div>
-        </div>
-      </form>
+      <div>
+        <h2>Add task</h2>
+        <form className="form">
+          <input className="form__field" placeholder="what to do?" autoFocus />
+          <button className="form__button">Add new task</button>
+        </form>
+      </div>
       <div>
         <div>
           <h2>Tasks list</h2>
-          <div className="section__buttons js-buttons"></div>
+          <div className="section__buttons"></div>
         </div>
-        <ul className="tasks js-tasks"></ul>
+        <ul className="tasks"></ul>
       </div>
-      <div className="js-stats"></div>
-      <button className="thumbUp js-thumbUp"></button>
-    </>
+      <div className="stats"></div>
+      <button className="thumbUp"></button>
+    </main>
   );
 }
 
