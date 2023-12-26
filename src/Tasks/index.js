afb306534b32a946edb4a5d78ba348d28f99517f
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-12-26 23:51:30 +0000

    Refactor Tasks component to include conditional rendering and styling

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,9 +1,16 @@
 import "./style.css";
 
-const Tasks = ({ tasks }) => (
+const Tasks = (props) => (
   <ul className="tasks">
-    {tasks.map((task) => (
-      <li key={task.id}>{task.content}</li>
+    {props.tasks.map((task) => (
+      <li
+        className={`list__item ${
+          task.done && props.hideDoneTasks ? "list__item--done" : ""
+        }${task.important ? " list__item--highLight" : ""}`}
+        key={task.id}
+      >
+        {task.content}
+      </li>
     ))}
   </ul>
 );
