6ada65cb8aed8848f0896e6500553379eee837b7
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-02-20 01:22:08 +0000

    watchFetchExampleTasks renamed to tasksSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,9 +1,9 @@
 import { all } from "redux-saga/effects";
-import { watchFetchExampleTasks } from "./features/tasks/Tasks/tasksSaga";
+import { tasksSaga } from "./features/tasks/Tasks/tasksSaga";
 
 export default function* rootSaga() {
   yield all([
-    watchFetchExampleTasks(),
+    tasksSaga(),
     // other sagas go here
   ]);
 }
