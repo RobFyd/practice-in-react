a85da4830d2e3d10da49a5ff2518e44f2129e620
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-02-20 01:18:27 +0000

    tasksSlice updated with localStorage

--- src/features/tasks/Tasks/tasksSlice.js
+++ src/features/tasks/Tasks/tasksSlice.js
@@ -1,15 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getTasksFromLocalStorage } from "./tasksLocalStorage";
 
 const tasksSlice = createSlice({
   name: "tasks",
   initialState: {
-    tasks: [
-      {
-        content: "work one",
-        done: true,
-        id: 1,
-      },
-    ],
+    tasks: getTasksFromLocalStorage(),
     hideDone: false,
   },
   reducers: {
