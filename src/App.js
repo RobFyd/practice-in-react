1c1ca90e3676b06cdc656759821f8612ad3301c6
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-02-21 01:14:08 +0000

    Add navigation and routing to App component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
 
-export const App = () => <Tasks />;
+export const App = () => (
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/tasks">Tasks</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/tasks">
+          <Tasks />
+        </Route>
+      </Switch>
+    </nav>
+  </BrowserRouter>
+);
