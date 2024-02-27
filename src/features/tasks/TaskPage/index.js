79f28bd258446397d542568a4d691444cd241d65
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-02-27 23:56:28 +0000

    useSelector hook to retrieve task by ID added

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -2,9 +2,12 @@ import Container from "../../../common/Container";
 import Header from "../../../common/Header";
 import Section from "../../../common/Section";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getTaskById } from "../tasksSlice";
 
 function TaskPage() {
   const { id } = useParams();
+  const task = useSelector((state) => getTaskById(state, id));
 
   return (
     <Container>
