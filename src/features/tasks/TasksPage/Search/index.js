5b49320084e9659005c3400234e3f6705a2b6d26
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-02-29 00:21:23 +0000

    search functionality in TasksPage refactored

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,14 +1,32 @@
 import Input from "../../Input";
 import { useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { Wrapper } from "./styled";
 
 export default () => {
-    const location = useLocation();
-    const query = new URLSearchParams(location.search).get("query");
+  const location = useLocation();
+  const history = useHistory();
+  const query = new URLSearchParams(location.search).get("search");
 
-    return (
-        <Input 
-            placeholder="search tasks"
-            // value={}
+  const onInputChange = ({ target }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    if (target.value.trim() === "") {
+      searchParams.delete("search");
+    } else {
+      searchParams.set("search", target.value);
+    }
+
+    history.push(`${location.pathname}?${searchParams.toString()}`);
+  };
+
+  return (
+    <Wrapper>
+        <Input
+            placeholder="Filter tasks"
+            value={query || ""}
+            onChange={onInputChange}
         />
-    )
+    </Wrapper>
+  );
 };
